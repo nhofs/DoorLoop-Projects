a1cb25228522aa69b871e80f0dd335ae2ae0d64d
nhofs <101490941+nhofs@users.noreply.github.com>
2023-03-16 21:19:15 -0400

    add js to change text and loading bar on file upload

--- docs/fileupload.js
+++ docs/fileupload.js
@@ -1,6 +1,9 @@
 onload = () => {
 const upload = document.getElementById('upload');
 const fileInput = document.getElementById("fileInput");
+const spaceLeft = document.getElementById("spaceLeft");
+const spaceUsed = document.getElementById("spaceUsed");
+const loadingWidth = document.getElementById("loadingWidth");
 upload.addEventListener("click", fileInputClick);
 fileInput.addEventListener("change", fileUpload);
 }
@@ -12,20 +15,44 @@ function fileInputClick() {
 
 function fileUpload() {
     const fileReader = new FileReader(); // initialize the object  
-    fileReader.readAsArrayBuffer(fileInput.files[0]); // read file as array buffer
+    for(let fileIndex = 0; fileIndex < fileInput.files.length; fileIndex++ ){
+    fileReader.readAsArrayBuffer(fileInput.files[fileIndex]);
+    // read file as array buffer
     fileReader.onload = async(event) => {
-        console.log("file read sucessful")
+        
 
-        // const content = event.target.result;
-        // const CHUNK_SIZE = 1000;
-        // const totalChunks = event.target.result.byteLength / CHUNK_SIZE;
+        const content = event.target.result;
+        const CHUNK_SIZE = 1024*1024;
+        const totalChunks = content.byteLength / CHUNK_SIZE;
+        if (totalChunks < 10){
+        console.log(totalChunks)
+        spaceUsed.innerHTML= Math.round(( totalChunks + Number.EPSILON) * 100) / 100 + " MB";
+        spaceLeft.innerHTML= Math.round(((10 - totalChunks) + Number.EPSILON) * 100) / 100;
+        let id = null;
+        let width=0;
+        clearInterval(id);
+        id = setInterval(frame, 10);
+        function frame() {if (width > totalChunks/.1)
+        {clearInterval(id);}
+        else {
+            width++;
+            loadingWidth.style.width= width + "%";
+        }
+        }
+        
+        loadingWidth.style.width = 30 + "%";
+        }
+        else
+        {alert('file exceeds maximum storage space')
+        }}
+        
 
         // // Generate filename
         // const fileName = Math.random().toString(36).slice(-6) + fileInput.files[0].name;
 
         // for (let chunk = 0; chunk < totalChunks + 1; chunk++){
         //     let CHUNK = content.slice(chunk * CHUNK_SIZE, (chunk + 1) * CHUNK_SIZE)
-        // }
+        
         // await fetch("/upload?fileName=" + fileName, {
         //     'method' : 'POST',
         //     'headers' : {
@@ -33,6 +60,7 @@ function fileUpload() {
         //         'content-length' : CHUNK.length,
         //     },
         //     'body' : CHUNK
-        // })
-    };
-}
+        // })}
+       
+    }
+};
